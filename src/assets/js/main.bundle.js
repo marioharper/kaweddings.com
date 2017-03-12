76a859d399b616ad280f93efeb6eea721bb0d5ba
marioharper <mario@marioharper.me>
2017-03-12 12:30:09 -0500

    menu design

--- src/assets/js/main.bundle.js
+++ src/assets/js/main.bundle.js
@@ -1,6 +1,6 @@
 const Nav = require('./modules/responsiveMenu');
 const classHelpers = require('./modules/classHelpers');
 
-var navElement = document.getElementById('main-menu');
+var navElement = document.getElementById('responsive-menu');
 
 window.nav = new Nav(navElement);
