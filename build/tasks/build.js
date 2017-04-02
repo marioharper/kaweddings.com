df5d2bea25df0e5e4c027f5a6906ecf051f2e041
marioharper <mario@marioharper.me>
2017-04-01 22:27:05 -0500

    gallery

--- build/tasks/build.js
+++ build/tasks/build.js
@@ -9,13 +9,45 @@ const gulp = require('gulp'),
   glob = require('glob'),
   rename = require('gulp-rename'),
   es = require('event-stream'),
-  autoprefixer = require('gulp-autoprefixer');
+  autoprefixer = require('gulp-autoprefixer'),
+  merge = require('merge-stream'),
+  concat = require('gulp-concat');
 
 // compiles nunjucks
 gulp.task('build-html', function () {
+  var galleries = {
+    'sara-and-daniel': {
+      name: 'Sarah & Daniel',
+      images: [
+        { large: { src: '/assets/images/gallery/sarah-and-daniel/1-1000x1500.jpg', width: 1000, height: 1500 }, small: { src: '/assets/images/gallery/sarah-and-daniel/1-300x450.jpg', width: 300, height: 450 } },
+        { large: { src: '/assets/images/gallery/sarah-and-daniel/2-1000x1500.jpg', width: 1000, height: 1500 }, small: { src: '/assets/images/gallery/sarah-and-daniel/2-300x450.jpg', width: 300, height: 450 } },
+        { large: { src: '/assets/images/gallery/sarah-and-daniel/3-1000x1500.jpg', width: 1000, height: 1500 }, small: { src: '/assets/images/gallery/sarah-and-daniel/3-300x450.jpg', width: 300, height: 450 } },
+        { large: { src: '/assets/images/gallery/sarah-and-daniel/4-1000x1500.jpg', width: 1000, height: 1500 }, small: { src: '/assets/images/gallery/sarah-and-daniel/4-300x450.jpg', width: 300, height: 450 } },
+        { large: { src: '/assets/images/gallery/sarah-and-daniel/5-1000x666.jpg', width: 1000, height: 666 }, small: { src: '/assets/images/gallery/sarah-and-daniel/5-300x200.jpg', width: 300, height: 200 } },
+        { large: { src: '/assets/images/gallery/sarah-and-daniel/6-1000x666.jpg', width: 1000, height: 666 }, small: { src: '/assets/images/gallery/sarah-and-daniel/6-300x200.jpg', width: 300, height: 200 } },
+        { large: { src: '/assets/images/gallery/sarah-and-daniel/7-1000x1500.jpg', width: 1000, height: 1500 }, small: { src: '/assets/images/gallery/sarah-and-daniel/7-300x450.jpg', width: 300, height: 450 } },
+        { large: { src: '/assets/images/gallery/sarah-and-daniel/8-1000x1500.jpg', width: 1000, height: 1500 }, small: { src: '/assets/images/gallery/sarah-and-daniel/8-300x450.jpg', width: 300, height: 450 } },
+        { large: { src: '/assets/images/gallery/sarah-and-daniel/9-1000x666.jpg', width: 1000, height: 666 }, small: { src: '/assets/images/gallery/sarah-and-daniel/9-300x200.jpg', width: 300, height: 200 } },
+        { large: { src: '/assets/images/gallery/sarah-and-daniel/10-1000x1500.jpg', width: 1000, height: 1500 }, small: { src: '/assets/images/gallery/sarah-and-daniel/10-300x450.jpg', width: 300, height: 450 } },
+        { large: { src: '/assets/images/gallery/sarah-and-daniel/11-1000x666.jpg', width: 1000, height: 666 }, small: { src: '/assets/images/gallery/sarah-and-daniel/11-300x200.jpg', width: 300, height: 200 } },
+        { large: { src: '/assets/images/gallery/sarah-and-daniel/12-1000x666.jpg', width: 1000, height: 666 }, small: { src: '/assets/images/gallery/sarah-and-daniel/12-300x200.jpg', width: 300, height: 200 } },
+        { large: { src: '/assets/images/gallery/sarah-and-daniel/13-1000x1500.jpg', width: 1000, height: 1500 }, small: { src: '/assets/images/gallery/sarah-and-daniel/13-300x450.jpg', width: 300, height: 450 } },
+        { large: { src: '/assets/images/gallery/sarah-and-daniel/14-1000x1499.jpg', width: 1000, height: 1499 }, small: { src: '/assets/images/gallery/sarah-and-daniel/14-300x449.jpg', width: 300, height: 449 } },
+        { large: { src: '/assets/images/gallery/sarah-and-daniel/15-1000x666.jpg', width: 1000, height: 666 }, small: { src: '/assets/images/gallery/sarah-and-daniel/15-300x200.jpg', width: 300, height: 200 } },
+        { large: { src: '/assets/images/gallery/sarah-and-daniel/16-1000x666.jpg', width: 1000, height: 666 }, small: { src: '/assets/images/gallery/sarah-and-daniel/16-300x200.jpg', width: 300, height: 200 } },
+        { large: { src: '/assets/images/gallery/sarah-and-daniel/17-1000x1500.jpg', width: 1000, height: 1500 }, small: { src: '/assets/images/gallery/sarah-and-daniel/17-300x450.jpg', width: 300, height: 450 } },
+        { large: { src: '/assets/images/gallery/sarah-and-daniel/18-1000x666.jpg', width: 1000, height: 666 }, small: { src: '/assets/images/gallery/sarah-and-daniel/18-300x200.jpg', width: 300, height: 200 } },
+        { large: { src: '/assets/images/gallery/sarah-and-daniel/19-1000x666.jpg', width: 1000, height: 666 }, small: { src: '/assets/images/gallery/sarah-and-daniel/19-300x200.jpg', width: 300, height: 200 } },
+        { large: { src: '/assets/images/gallery/sarah-and-daniel/20-1000x1500.jpg', width: 1000, height: 1500 }, small: { src: '/assets/images/gallery/sarah-and-daniel/20-300x450.jpg', width: 300, height: 450 } }
+      ]
+    }};
+
   return gulp.src(paths.html)
     .pipe(nunjucksRender({
-      path: ['src']
+      path: ['src'],
+      data: {
+        galleries: galleries
+      }
     }))
     .pipe(gulp.dest(paths.output));
 });
@@ -45,19 +77,41 @@ gulp.task('build-js', function (done) {
 });
 
 gulp.task('build-css', function () {
-  return gulp.src(paths.sass)
+  var cssStream = gulp.src(paths.css)
+    .pipe(concat('css-files.css'));
+
+  var sassStream = gulp.src(paths.sass)
     .pipe(sass({
       outputStyle: 'compressed'
     }).on('error', sass.logError))
-    .pipe(autoprefixer())
+    .pipe(autoprefixer());
+
+  var mergedStream = merge(sassStream, cssStream)
+    .pipe(concat('main.css'))
     .pipe(gulp.dest(paths.output + "assets/styles/"))
     .pipe(browserSync.stream());
+
+  return mergedStream;
+});
+
+gulp.task('build-copy', ['build-copy-photoswipe', 'build-copy-lazyload'], function () {
+  return
+});
+
+gulp.task('build-copy-lazyload', function () {
+  return gulp.src(['node_modules/vanilla-lazyload/dist/lazyload.min.js'])
+    .pipe(gulp.dest(paths.output + "assets/vendor/vanilla-lazyload"));
+});
+
+gulp.task('build-copy-photoswipe', function () {
+  return gulp.src(['node_modules/photoswipe/dist/**/*'])
+    .pipe(gulp.dest(paths.output + "assets/vendor/photoswipe"));
 });
 
 gulp.task('build', function (callback) {
   return runSequence(
     'clean',
-    ['build-html', 'build-img', 'build-js', 'build-css'],
+    ['build-html', 'build-img', 'build-js', 'build-css', 'build-copy'],
     callback
   );
 });
--- src/assets/js/main.bundle.js
+++ src/assets/js/main.bundle.js
@@ -2,5 +2,4 @@ const Nav = require('./modules/responsiveMenu');
 const classHelpers = require('./modules/classHelpers');
 
 var navElement = document.getElementById('responsive-menu');
-
 window.nav = new Nav(navElement);
